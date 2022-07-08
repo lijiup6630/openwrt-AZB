fc50fa0a684d6c12bcb7e08cc52f3f62109e7a42
李季 <liji6630@foxmail.com>
2022-07-08 15:17:46 +0800

    target profile增加AZB模板

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -7,4 +7,17 @@ define Device/generic
 	kmod-tg3
   GRUB2_VARIANT := generic
 endef
+
 TARGET_DEVICES += generic
+
+define Device/AZB
+  DEVICE_VENDOR := AZB
+  DEVICE_MODEL := x86/64
+  DEVICE_PACKAGES += \
+	kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-e1000e kmod-e1000 \
+	kmod-forcedeth kmod-fs-vfat kmod-igb kmod-ixgbe kmod-r8169 \
+	kmod-tg3
+  GRUB2_VARIANT := generic
+endef
+
+TARGET_DEVICES += AZB
