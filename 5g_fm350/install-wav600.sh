24dd15be2dc16ff740e8b490b92410b19d2d905e
liji <liji@huachen.link>
2023-07-10 11:04:07 +0800

    modify wav600 luci no ax

---
+++ 5g_fm350/install-wav600.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp luci-app-wireless/network.js ../feeds/luci/modules/luci-base/htdocs/luci-static/resources/
+cp luci-app-wireless/wireless.js ../feeds/luci/modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
--- 5g_fm350/install.sh
+++ 5g_fm350/install.sh
@@ -3,3 +3,4 @@ cp luci-app-lte5g/ -rf ../feeds/luci/applications/
 cd ../package/feeds/luci/
 ln -s ../../../feeds/luci/applications/luci-app-lte5g/ luci-app-lte5g
 
+
